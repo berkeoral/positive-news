2c80e07387f90f9ca2e2743adca9a85d58f28a85
berke <berkeoral>
2018-05-17 00:20:48 +0300

    Added max and min inter crawl time to crawler

--- backend/crawler/crawler.py
+++ backend/crawler/crawler.py
@@ -1,45 +1,56 @@
 import sys
+import time
 
 import newspaper
 
 from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 from tqdm import tqdm
 
 from backend.utils.utils import Utils
 from backend.crawler.news_sources.source import Sources
 from backend.utils.txtops import TextOps
 
-
+max_interval = 10*60.
 
 class Crawler:
-    def __init__(self):
+    def __init__(self, max_interval = 10*60.):
         self.papers = []
         self.textops = TextOps()
+        self.max_interval = max_interval
+        self.start_time = time.time()
 
-    def crawl(self):
-        self.__init_papers()
-        self.__start_crawl()
+    def crawl(self, debug_mode=-1):
+        self._init_papers(debug_mode)
+        return self._start_crawl()
 
-    def __init_papers(self):
+    def _init_papers(self, debug_mode):
         source_urls = Sources().get_sources()
-        for source_url in tqdm(source_urls, file=sys.stdout):
+        for i, source_url in tqdm(enumerate(source_urls), file=sys.stdout,
+                                  total=(debug_mode if debug_mode > 0 else len(source_urls))):
             try:
-                tqdm.write("Initialising paper: " + source_url)
+                if i == debug_mode:
+                    return
+                tqdm.write("Initialising paper: " + source_url, file=sys.stdout)
                 paper = newspaper.build(source_url,
                                         memoize_articles=True,
                                         keep_article_html=True,
-                                        fetch_images=False)
+                                        fetch_images=True)
                 # Downloads non-english categories, bad solution
                 #   > sometimes prefix contains country code
                 paper.categories = [category for category in paper.categories
                                     if Utils().is_eng_suffix(None, category.url)]
                 #       > not conclusive
                 self.papers.append(paper)
+                if time.time() - self.start_time > self.max_interval:
+                    break
             except:
                 print('An error occurred.')
 
-    def __start_crawl(self):
+    def _start_crawl(self):
+        articles = []
         tqdm.write("Initialized \" " + str(self.papers.__len__()) + " \" papers, starting to crawl articles")
+        self.start_time = time.time()
         for paper in tqdm(self.papers, file=sys.stdout):
             paper.articles = [article for article in paper.articles
                               if Utils().is_eng_suffix(None, article.url)
@@ -48,9 +59,14 @@ class Crawler:
                 try:
                     article.build()
                     if detect(article.text) == "en":
+                        tqdm.write(article.title, file=sys.stdout)
+                        articles.append([article.url, article.top_image, article.title, article.text])
                         self.textops.append_records(article.url, article.title, article.text)
-                        tqdm.write(article.title)
+                except LangDetectException:
+                    continue
                 except:
-                    pass
+                    continue
+                if time.time() - self.start_time > self.max_interval:
+                    break
 
-
+        return articles
